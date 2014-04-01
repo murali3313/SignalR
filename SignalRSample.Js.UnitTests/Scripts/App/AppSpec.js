0467e01ac0e48d1d9442dc00213a7b3bc69f0c6e
muralin <muralin@thoughtworks.com>
2014-04-01 13:25:39 +0530

    Adding Karma support for jasmine

---
+++ SignalRSample.Js.UnitTests/Scripts/App/AppSpec.js
@@ -0,0 +1,7 @@
+﻿describe("Test Unit Test", function() {
+    it("Test", function () {
+        var s = spyOn($, "connection");
+        var signalRChat = new SignalRChat(s);
+        expect(1).toBe(1);
+    });
+});
--- SignalRSample/Scripts/App/app.js
+++ SignalRSample/Scripts/App/app.js
@@ -1,2 +1,32 @@
-function hello(){
-}
+var SignalRChat = function (connection) {
+    this.chat = connection.chat;
+    this.chatHub = connection.hub;
+};
+
+SignalRChat.prototype = {
+    initialize: function() {
+        var that = this;
+        that.chat.client.addMessage = function(messageFromServer) {
+            that.AppendVaueToHistory(messageFromServer);
+        };
+        that.chatHub.start().done(function() {
+            that.AppendVaueToHistory("Connected");
+        });
+    },
+    postMessage: function(message) {
+        this.chat.server.send(message);
+    },
+    AppendVaueToHistory: function(message) {
+        var chatHistory = $("#chatHistory");
+        var chatHistoryValue = chatHistory.val();
+        chatHistoryValue += message + "\r\n";
+        chatHistory.val(chatHistoryValue);
+    }
+
+};
+//var chat;
+//$(function () {
+//    chat = new SignalRChat($.connection);
+//    chat.initialize();
+//});
+
